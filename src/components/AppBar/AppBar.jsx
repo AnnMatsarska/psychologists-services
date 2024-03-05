438a02e00219950184b7389d7f2705c449012621
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-05 11:20:54 +0200

    add logo

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,6 +5,9 @@ export const AppBar = () => {
   return (
     <header className={css.header}>
       <div className={css.container}>
+        <p className={css.logoText}>
+          <span className={css.logoSpan}>psychologists.</span>services
+        </p>
         <Navigation />
       </div>
     </header>
