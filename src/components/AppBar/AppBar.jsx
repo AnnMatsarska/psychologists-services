46480a41e9525977ce31dcc90646b2bcd5a9cbe5
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-05 18:41:31 +0200

    add active class for nav menu

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,15 +1,18 @@
 import { Navigation } from 'components/Navigation/Navigation';
 import css from './AppBar.module.css';
 import { UserAuth } from 'components/UserAuth/UserAuth';
+import { NavLink } from 'react-router-dom';
 
 export const AppBar = () => {
   return (
     <header className={css.header}>
       <div className={css.container}>
         <nav className={css.navWrapper}>
-          <p className={css.logoText}>
-            <span className={css.logoSpan}>psychologists.</span>services
-          </p>
+          <NavLink to="/">
+            <p className={css.logoText}>
+              <span className={css.logoSpan}>psychologists.</span>services
+            </p>
+          </NavLink>
           <Navigation />
           <UserAuth />
         </nav>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,21 +2,22 @@ import { NavLink } from 'react-router-dom';
 import css from './Navigation.module.css';
 
 export const Navigation = () => {
+  const setAtive = ({ isActive }) => (isActive ? css.active : css.linkItem);
   return (
     <>
       <ul className={css.list}>
         <li>
-          <NavLink to="/" className={css.linkItem}>
+          <NavLink to="/" className={setAtive}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/psychologists" className={css.linkItem}>
+          <NavLink to="/psychologists" className={setAtive}>
             Psychologists
           </NavLink>
         </li>
         <li>
-          <NavLink to="/favorites" className={css.linkItem}>
+          <NavLink to="/favorites" className={setAtive}>
             Favorites
           </NavLink>
         </li>
