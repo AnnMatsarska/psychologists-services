732a98022c8c1615f2120d568f8d3e6dc107fd14
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-05 20:01:16 +0200

    add home page

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,15 +2,18 @@ import { AppBar } from 'components/AppBar/AppBar';
 import { Loader } from 'components/Loader/Loader';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import css from './SharedLayout.module.css';
 
 export const SharedLayout = () => {
   return (
     <>
       <AppBar />
       <main>
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
+        <div className={css.container}>
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </div>
       </main>
     </>
   );
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,5 +1,27 @@
+import homeImg from '../../images/home-image-min.png';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
-  return <p>HomePage</p>;
+  return (
+    <section className={css.section}>
+      <div>
+        <h1 className={css.sectionTitle}>
+          The road to the <span className={css.spanText}>depths</span> of the
+          human soul
+        </h1>
+        <p className={css.sectionText}>
+          We help you to reveal your potential, overcome challenges and find a
+          guide in your own life with the help of our experienced psychologists.
+        </p>
+        <button type="button" className={css.button}>
+          Get started
+        </button>
+      </div>
+      <div>
+        <img src={homeImg} className={css.homeImg} alt="home img"></img>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
