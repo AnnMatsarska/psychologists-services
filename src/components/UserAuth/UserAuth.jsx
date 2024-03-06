b315725caa51d5a12c8c63f6f72c4ef655378081
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-06 20:55:48 +0200

    add modal component

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,36 +1,49 @@
 import { useState } from 'react';
 import css from './UserAuth.module.css';
+import Modal from 'components/Modal/Modal';
 
 export const UserAuth = () => {
   const [activeButton, setActiveButton] = useState(null);
+  const [isOpen, setTeamModalOpen] = useState(false);
 
   const handleButtonClick = button => {
     setActiveButton(button);
+    setTeamModalOpen(true);
+    document.body.classList.add('body-scroll-lock');
+  };
+
+  const closeModal = () => {
+    setTeamModalOpen(false);
+    document.body.classList.remove('body-scroll-lock');
+    setActiveButton(null);
   };
   return (
-    <ul className={css.list}>
-      <li>
-        <button
-          type="button"
-          className={`${css.linkItem} ${
-            activeButton === 'login' ? css.active : css.linkItem
-          }`}
-          onClick={() => handleButtonClick('login')}
-        >
-          Log In
-        </button>
-      </li>
-      <li>
-        <button
-          type="button"
-          className={`${css.linkItem} ${
-            activeButton === 'registration' ? css.active : css.linkItem
-          }`}
-          onClick={() => handleButtonClick('registration')}
-        >
-          Registration
-        </button>
-      </li>
-    </ul>
+    <>
+      <ul className={css.list}>
+        <li>
+          <button
+            type="button"
+            className={`${css.linkItem} ${
+              activeButton === 'login' ? css.active : css.linkItem
+            }`}
+            onClick={() => handleButtonClick('login')}
+          >
+            Log In
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={`${css.linkItem} ${
+              activeButton === 'registration' ? css.active : css.linkItem
+            }`}
+            onClick={() => handleButtonClick('registration')}
+          >
+            Registration
+          </button>
+        </li>
+      </ul>
+      {isOpen && <Modal onClose={closeModal}></Modal>}
+    </>
   );
 };
