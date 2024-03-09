aece1f0ac41855d4ef61343eda77adda251bdc6e
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-10 00:12:53 +0200

    add persisting logic for authenticated user

---
+++ src/components/AuthContainer.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../redux/auth/authSlice';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase/config';
+
+const AuthContainer = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        dispatch(setUser({ id: user.uid, email: user.email }));
+      } else {
+        dispatch(setUser(null));
+      }
+    });
+
+    return () => unsubscribe(); // Cleanup the listener on component unmount
+  }, [dispatch]);
+
+  return null; // No need to render anything in this higher-level component
+};
+
+export default AuthContainer;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,10 +3,12 @@ import { Loader } from 'components/Loader/Loader';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import css from './SharedLayout.module.css';
+import AuthContainer from '../AuthContainer';
 
 export const SharedLayout = () => {
   return (
     <>
+      <AuthContainer />
       <div className={css.layout}>
         <AppBar />
         <main>
