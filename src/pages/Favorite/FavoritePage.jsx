fa328c2802e3d89e88f6d224297baee3617fb3f5
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-11 23:44:52 +0200

    fix styles for page favorites

--- src/pages/Favorite/FavoritePage.jsx
+++ src/pages/Favorite/FavoritePage.jsx
@@ -1,10 +1,14 @@
 import { FavoritesList } from 'components/FavouritesList/FavoritesList';
 
+import css from './FavoritePage.module.css';
+
 const FavoritePage = () => {
   return (
-    <>
-      <FavoritesList />
-    </>
+    <section className={css.section}>
+      <div className={css.container}>
+        <FavoritesList />
+      </div>
+    </section>
   );
 };
 
