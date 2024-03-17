2fbfb8c82578dc8df73a7b41664ae347714239d4
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-17 23:12:42 +0200

    refactor dropdown component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,29 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './Filter.module.css';
 import { Dropdown } from '../Dropdown/Dropdown';
+import { ReactComponent as IconDropdown } from '../../images/chevron-down.svg';
 import { useDispatch } from 'react-redux';
 import { setFilter } from '../../redux/psychologists/slice';
 
 export const Filter = () => {
-  const defaultFilter = 'Show all';
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('Show all');
+
   const dispatch = useDispatch();
 
-  const handleFilterChange = selectedFilter => {
-    dispatch(setFilter(selectedFilter));
+  const handleOptionClick = option => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    dispatch(setFilter(option));
   };
 
   return (
     <div>
       <label className={css.filterLabel}>Filters</label>
-      <Dropdown defaultOption={defaultFilter} onSelect={handleFilterChange}>
-        <span value="A to Z">A to Z</span>
-        <span value="Z to A">Z to A</span>
-        <span value="Less than 10$">Less than 10$</span>
-        <span value="Greater than 10$">Greater than 10$</span>
-        <span value="Popular">Popular</span>
-        <span value="Not popular">Not popular</span>
-        <span value="Show all">Show all</span>
-      </Dropdown>
+      <div className={css.dropdown}>
+        <button
+          className={css.dropBtn}
+          onClick={() => setIsOpen(!isOpen)}
+          type="button"
+        >
+          {selectedOption}
+          <IconDropdown className={isOpen ? css.rotatedIcon : null} />
+        </button>
+        {isOpen && (
+          <Dropdown close={() => setIsOpen(false)}>
+            <ul className={css.list}>
+              <li onClick={() => handleOptionClick('A to Z')} value="A to Z">
+                A to Z
+              </li>
+              <li onClick={() => handleOptionClick('Z to A')} value="Z to A">
+                Z to A
+              </li>
+              <li
+                onClick={() => handleOptionClick('Less than 10$')}
+                value="Less than 10$"
+              >
+                Less than 10$
+              </li>
+              <li
+                onClick={() => handleOptionClick('Greater than 10$')}
+                value="Greater than 10$"
+              >
+                Greater than 10$
+              </li>
+              <li onClick={() => handleOptionClick('Popular')} value="Popular">
+                Popular
+              </li>
+              <li
+                onClick={() => handleOptionClick('Not popular')}
+                value="Not popular"
+              >
+                Not popular
+              </li>
+              <li
+                onClick={() => handleOptionClick('Show all')}
+                value="Show all"
+              >
+                Show all
+              </li>
+            </ul>
+          </Dropdown>
+        )}
+      </div>
     </div>
   );
 };
