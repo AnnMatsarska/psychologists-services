e48ff50180acab31d33af3367f28fef4c1962b96
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-17 23:49:38 +0200

    back previous comit

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,17 +1,28 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { ReactComponent as IconDropdown } from '../../images/chevron-down.svg';
+import css from './Dropdown.module.css';
 
-export const Dropdown = ({ close, children }) => {
+export const Dropdown = ({ defaultOption, onSelect, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(defaultOption);
   const dropdownRef = useRef(null);
 
+  const handleOptionClick = option => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    onSelect(option);
+  };
+
+  const handleClickOutside = event => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
   useEffect(() => {
-    const handleClickOutside = event => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        close();
-      }
-    };
     const closeDropdownEsc = ({ code }) => {
       if (code === 'Escape') {
-        close();
+        setIsOpen(false);
       }
     };
     window.addEventListener('keydown', closeDropdownEsc);
@@ -20,7 +31,31 @@ export const Dropdown = ({ close, children }) => {
       document.removeEventListener('mousedown', handleClickOutside);
       window.removeEventListener('keydown', closeDropdownEsc);
     };
-  }, [close]);
+  }, []);
 
-  return <div ref={dropdownRef}>{children}</div>;
+  useEffect(() => {
+    setSelectedOption(defaultOption);
+  }, [defaultOption]);
+
+  return (
+    <div className={css.dropdown} ref={dropdownRef}>
+      <button className={css.dropBtn} onClick={() => setIsOpen(!isOpen)}>
+        {selectedOption}
+        <IconDropdown className={isOpen ? css.rotatedIcon : null} />
+      </button>
+
+      {isOpen && (
+        <ul className={css.list}>
+          {React.Children.map(children, child => (
+            <li
+              key={child.props.value}
+              onClick={() => handleOptionClick(child.props.value)}
+            >
+              {child}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
