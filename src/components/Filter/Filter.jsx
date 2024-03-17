442defe10affe45030b3ea726e70719f93ebc600
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-17 23:42:37 +0200

    refactor dropdown component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -7,6 +7,15 @@ import { setFilter } from '../../redux/psychologists/slice';
 export const Filter = () => {
   const defaultFilter = 'Show all';
   const dispatch = useDispatch();
+  const options = [
+    'A to Z',
+    'Z to A',
+    'Less than 10$',
+    'Greater than 10$',
+    'Popular',
+    'Not popular',
+    'Show all',
+  ];
 
   const handleFilterChange = selectedFilter => {
     dispatch(setFilter(selectedFilter));
@@ -16,13 +25,13 @@ export const Filter = () => {
     <div>
       <label className={css.filterLabel}>Filters</label>
       <Dropdown defaultOption={defaultFilter} onSelect={handleFilterChange}>
-        <span value="A to Z">A to Z</span>
-        <span value="Z to A">Z to A</span>
-        <span value="Less than 10$">Less than 10$</span>
-        <span value="Greater than 10$">Greater than 10$</span>
-        <span value="Popular">Popular</span>
-        <span value="Not popular">Not popular</span>
-        <span value="Show all">Show all</span>
+        {options.map(option => {
+          return (
+            <span key={option} value={option}>
+              {option}
+            </span>
+          );
+        })}
       </Dropdown>
     </div>
   );
