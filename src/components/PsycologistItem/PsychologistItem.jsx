6d8d540df81649c202d865a520492d65e640acc2
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-26 22:27:54 +0200

    fix img

--- src/components/PsycologistItem/PsychologistItem.jsx
+++ src/components/PsycologistItem/PsychologistItem.jsx
@@ -53,8 +53,6 @@ export const PsychologistItem = ({ psychologist }) => {
           className={css.itemImage}
           src={psychologist.avatar_url}
           alt={psychologist.name}
-          width={96}
-          height={96}
         />
       </div>
       <div style={{ width: '100%' }}>
